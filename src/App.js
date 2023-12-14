d352bfb2aff4d0e402976e0f2d6d24e8f84e53eb
Dayeon Lee <iamdayeonlee@gmail.com>
2023-12-14 16:52:01 +0000

    Add project rock paper scissors game: understanding JSX, prop, state, css styling in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Button from "./Button";
+import HandButton from "./HandButton";
+import { compareHand, generateRandomHand } from "./utils";
+import HandIcon from "./HandIcon";
+import "./HandButton.css";
+
+function getResult(me, other) {
+  const comparison = compareHand(me, other);
+  if (comparison > 0) return "won";
+  if (comparison < 0) return "lost";
+  return "drew";
+}
 
 function App() {
+  const [hand, setHand] = useState("rock");
+  const [otherHand, setotherHand] = useState("rock");
+  const [gameHistory, setgameHistory] = useState(["drew"]);
+  const currentResult = gameHistory[gameHistory.length - 1];
+  const [bet, setBet] = useState(1);
+  const [myScore, setmyScore] = useState(0);
+  const [otherScore, setotherScore] = useState(0);
+
+  const handleBetChange = (e) => {
+    setBet(e.target.value);
+  };
+
+  const handleButtonClick = (nextHand) => {
+    setHand(nextHand);
+    const nextOtherHand = generateRandomHand();
+    setotherHand(nextOtherHand);
+
+    const nextHistoryItem = getResult(nextHand, nextOtherHand);
+    setgameHistory([...gameHistory, nextHistoryItem]);
+    if (nextHistoryItem === "won") {
+      const sum = myScore + Number(bet);
+      setmyScore(sum);
+    } else if (nextHistoryItem === "lost") {
+      const sum = otherScore + Number(bet);
+      setotherScore(sum);
+    }
+  };
+
+  const handleClearClick = () => {
+    console.log("reset");
+    setHand("rock");
+    setotherHand("rock");
+    setgameHistory([]);
+    setmyScore(0);
+    setotherScore(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="Box">
+      <h1 className="Heading">
+        Rock paper scissors <br /> 👊 🖐️ ✌️{" "}
+      </h1>
+      <div className="BtnBox">
+        <Button onClick={handleClearClick}>Reset</Button>
+      </div>
+      <div className="ScoreBox">
+        <p>Result: {currentResult}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Score: {myScore} : {otherScore}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Bet: {bet}</p>
+        <span> Score weight: </span>
+        <input type="number" min="1" max="5" onChange={handleBetChange}></input>
+        <p>Result History: {gameHistory.join(", ")}</p>
+      </div>
+      <div>
+        <HandIcon value={hand} result="result" />
+        <span>VS</span>
+        <HandIcon value={otherHand} result="result" />
+      </div>
+      <hr></hr>
+      <div className="ChoiceBox">
+        <HandButton onClick={handleButtonClick} value="rock" choice="choice" />
+        <HandButton
+          onClick={handleButtonClick}
+          value="scissor"
+          choice="choice"
+        />
+        <HandButton onClick={handleButtonClick} value="paper" choice="choice" />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Fragment } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<h1> react</h1>);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const WINS = {
+//   rock: "scissor",
+//   scissor: "paper",
+//   paper: "rock",
+// };
+
+// function getResult(left, right) {
+//   if (WINS[left] === right) return "won";
+//   else if (left === WINS[right]) return "lost";
+//   else return "draw";
+// }
+
+// function handleClick() {
+//   console.log("rock scissor paper!");
+// }
+
+// const me = "paper";
+// const other = "rock";
+// const result = getResult(me, other);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//     <h1 id="title">가위바위보</h1>
+//     <h2>{result}</h2>
+//     <button onClick={handleClick}>가위</button>
+//     <button onClick={handleClick}>보</button>
+//     <button onClick={handleClick}>바위</button>
+//   </>
+// );
